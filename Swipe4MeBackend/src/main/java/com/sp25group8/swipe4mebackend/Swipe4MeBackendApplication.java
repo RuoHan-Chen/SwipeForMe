7ce476f93a0a2f59731d9aebb696c7330659cb7f
Steven Yi <stevenjxhc@gmail.com>
2025-01-23 10:30:04 -0600

    initial commit

---
+++ Swipe4MeBackend/src/main/java/com/sp25group8/swipe4mebackend/Swipe4MeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sp25group8.swipe4mebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swipe4MeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swipe4MeBackendApplication.class, args);
+    }
+
+}
---
+++ Swipe4MeBackend/src/test/java/com/sp25group8/swipe4mebackend/Swipe4MeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp25group8.swipe4mebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swipe4MeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
