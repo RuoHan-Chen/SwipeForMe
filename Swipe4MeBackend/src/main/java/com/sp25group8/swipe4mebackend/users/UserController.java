81429bcea13635edf4415daee0a54a0593b43ef0
Steven Yi <stevenjxhc@gmail.com>
2025-02-04 18:08:45 -0600

    delete unused imports

--- Swipe4MeBackend/src/main/java/com/sp25group8/swipe4mebackend/users/UserController.java
+++ Swipe4MeBackend/src/main/java/com/sp25group8/swipe4mebackend/users/UserController.java
@@ -1,11 +1,7 @@
 package com.sp25group8.swipe4mebackend.users;
 
 import com.sp25group8.swipe4mebackend.users.dtos.UserDto;
-import com.sp25group8.swipe4mebackend.users.models.RegisterBody;
-import com.sp25group8.swipe4mebackend.users.models.UserEntity;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
