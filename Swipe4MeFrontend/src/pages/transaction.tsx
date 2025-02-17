5ef049bf27a86239798238fadb113b5051774c2d
RuoHan-Chen <jtafah@gmail.com>
2025-02-17 00:26:12 -0600

    index on main: ef02337 Merge pull request #3 from RuoHan-Chen/token-based-auth

---
+++ Swipe4MeFrontend/src/pages/transaction.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Transaction: React.FC = () => {
+  return (
+    <div className="filler-container">
+      <h1 className="filler-title">Transaction Coming Soon</h1>
+      <p className="filler-text">This page is under construction. Check back later!</p>
+    </div>
+  );
+};
+
+export default Transaction;
