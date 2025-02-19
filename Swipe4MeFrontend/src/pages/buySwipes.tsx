499bd81e724cb517e332efcef9d72789519244ca
Steven Yi <stevenjxhc@gmail.com>
2025-02-19 17:15:40 -0600

    table pagination

--- Swipe4MeFrontend/src/pages/buySwipes.tsx
+++ Swipe4MeFrontend/src/pages/buySwipes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
@@ -7,9 +7,12 @@ import TableContainer from "@mui/material/TableContainer";
 import Paper from "@mui/material/Paper";
 import TableBody from "@mui/material/TableBody";
 import Button from "@mui/material/Button";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
 
 const dummyData = [
   {
+    id: 1,
     name: "Jane Cooper",
     hall: "Rand",
     time: "12:30-13:30",
@@ -18,6 +21,7 @@ const dummyData = [
     action: "Send Invite",
   },
   {
+    id: 2,
     name: "Floyd Miles",
     hall: "Rand",
     time: "10:00-12:00",
@@ -26,6 +30,7 @@ const dummyData = [
     action: "Pending",
   },
   {
+    id: 3,
     name: "Ronald Richards",
     hall: "Rand",
     time: "10:00-12:00",
@@ -34,6 +39,7 @@ const dummyData = [
     action: "Pending",
   },
   {
+    id: 4,
     name: "Marvin McKinney",
     hall: "Rothchild",
     time: "12:30-13:30",
@@ -42,6 +48,7 @@ const dummyData = [
     action: "Send Invite",
   },
   {
+    id: 5,
     name: "Jerome Bell",
     hall: "Commons",
     time: "10:00-12:00",
@@ -50,6 +57,7 @@ const dummyData = [
     action: "Send Invite",
   },
   {
+    id: 6,
     name: "Kathryn Murphy",
     hall: "Commons",
     time: "12:30-13:30",
@@ -58,6 +66,7 @@ const dummyData = [
     action: "Send Invite",
   },
   {
+    id: 7,
     name: "Jacob Jones",
     hall: "Rand",
     time: "12:30-13:30",
@@ -65,9 +74,76 @@ const dummyData = [
     rating: 3.0,
     action: "Send Invite",
   },
+  {
+    id: 8,
+    name: "Savannah Nguyen",
+    hall: "Rand",
+    time: "14:00-15:00",
+    email: "savannah@vanderbilt.edu",
+    rating: 4.2,
+    action: "Send Invite",
+  },
+  {
+    id: 9,
+    name: "Cameron Williamson",
+    hall: "Rothchild",
+    time: "11:00-12:00",
+    email: "cameron@vanderbilt.edu",
+    rating: 3.8,
+    action: "Pending",
+  },
+  {
+    id: 10,
+    name: "Leslie Alexander",
+    hall: "Commons",
+    time: "09:00-10:00",
+    email: "leslie@vanderbilt.edu",
+    rating: 4.9,
+    action: "Send Invite",
+  },
+  {
+    id: 11,
+    name: "Courtney Henry",
+    hall: "Rand",
+    time: "13:00-14:00",
+    email: "courtney@vanderbilt.edu",
+    rating: 3.5,
+    action: "Pending",
+  },
+  {
+    id: 12,
+    name: "Arlene McCoy",
+    hall: "Rothchild",
+    time: "15:00-16:00",
+    email: "arlene@vanderbilt.edu",
+    rating: 4.6,
+    action: "Send Invite",
+  },
+  {
+    id: 13,
+    name: "Guy Hawkins",
+    hall: "Commons",
+    time: "10:30-11:30",
+    email: "guy@vanderbilt.edu",
+    rating: 3.9,
+    action: "Pending",
+  },
+  {
+    id: 14,
+    name: "Kristin Watson",
+    hall: "Rand",
+    time: "12:00-13:00",
+    email: "kristin@vanderbilt.edu",
+    rating: 4.3,
+    action: "Send Invite",
+  },
 ];
 
+const ROWS_PER_PAGE = 8;
+
 const buySwipes: React.FC = () => {
+  const [page, setPage] = useState(0);
+
   return (
     <div className="filler-container">
       <TableContainer
@@ -86,19 +162,34 @@ const buySwipes: React.FC = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {dummyData.map((row, index) => (
-              <TableRow key={index}>
-                <TableCell>{row.name}</TableCell>
-                <TableCell>{row.hall}</TableCell>
-                <TableCell>{row.time}</TableCell>
-                <TableCell>{row.email}</TableCell>
-                <TableCell>{row.rating}</TableCell>
-                <TableCell>
-                  <Button>{row.action}</Button>
-                </TableCell>
-              </TableRow>
-            ))}
+            {dummyData
+              .slice(page * ROWS_PER_PAGE, page * ROWS_PER_PAGE + ROWS_PER_PAGE)
+              .map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.hall}</TableCell>
+                  <TableCell>{row.time}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{row.rating}</TableCell>
+                  <TableCell>
+                    <Button>{row.action}</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[8]}
+                count={dummyData.length}
+                rowsPerPage={8}
+                page={page}
+                onPageChange={(_, newPage) => {
+                  setPage(newPage);
+                }}
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       </TableContainer>
     </div>
