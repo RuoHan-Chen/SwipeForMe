79ac0caa2baaa9d77064860e9ea490f223a5af92
1506599306 <jerrywei2004731@gmail.com>
2025-02-20 15:59:31 -0600

    profile UI

--- Swipe4MeFrontend/src/pages/profile.tsx
+++ Swipe4MeFrontend/src/pages/profile.tsx
@@ -1,43 +1,26 @@
 import React from "react";
-import "../styles/profile.css"; // Import styles
+import "../styles/profile.css";
+import ProfileHeader from "../pages/profileHeader";
+import Rating from "../components/rating";
+import MealSwipeCalculator from "../components/mealSwipeCalculator";
 
-const Profile = () => {
-    return (
-        <div className="profile-container">
-            {/* Profile Section */}
-            <div className="profile-header">
-                {/* Profile Picture */}
-                <div className="profile-photo-container">
-                    <img src="/profile_pic.png" alt="Profile" className="profile-photo" />
-                </div>
+const Profile: React.FC = () => {
+  return (
+    <div className="profile-container">
+      {/* Profile 头部信息，独占整行 */}
+      <ProfileHeader />
 
-                {/* User Details */}
-                <div className="profile-details">
-                    <div className="profile-header-row">
-                        <h2 className="user-name">RuoHan Chen</h2>
-                        <div className="profile-actions">
-                            <img src="/notification-icon.png" alt="Notification" className="notification-icon" />
-                            <button className="edit-profile-btn">Edit Profile</button>
-                        </div>
-                    </div>
-
-                    {/* User Info Rows */}
-                    <div className="profile-info">
-                        <div className="profile-column">
-                            <p><strong>Email:</strong> Ruohan@gmail.com</p>
-                            <p><strong>Class:</strong> Sophomore</p>
-                        </div>
-                        <div className="profile-column">
-                            <p><strong>Age:</strong> 21</p>
-                            <p><strong>Phone Number:</strong> +1 7306185390</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      {/* 评分 + Meal Swipe Calculator */}
+      <div className="profile-sections">
+        <div className="rating-container">
+          <Rating />
         </div>
-    );
+        <div className="calculator-container">
+          <MealSwipeCalculator />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
-
-
