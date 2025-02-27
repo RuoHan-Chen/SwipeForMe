0103402e7e5efcb4c2e4b78ccdd41cc11697ac2e
Steven Yi <stevenjxhc@gmail.com>
2025-02-26 20:04:07 -0600

    add table annotation

--- Swipe4MeBackend/src/main/java/com/sp25group8/swipe4mebackend/transactions/models/TransactionEntity.java
+++ Swipe4MeBackend/src/main/java/com/sp25group8/swipe4mebackend/transactions/models/TransactionEntity.java
@@ -1,9 +1,11 @@
 package com.sp25group8.swipe4mebackend.transactions.models;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
+@Table("transactions")
 public record TransactionEntity(
         @Id
         Long id,
