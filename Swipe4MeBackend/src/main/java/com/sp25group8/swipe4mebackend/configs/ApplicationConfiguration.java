b418d751139a44cd3c0b0ea61264779530a1133c
Steven Yi <stevenjxhc@gmail.com>
2025-03-05 19:15:56 -0600

    create email service

--- Swipe4MeBackend/src/main/java/com/sp25group8/swipe4mebackend/configs/ApplicationConfiguration.java
+++ Swipe4MeBackend/src/main/java/com/sp25group8/swipe4mebackend/configs/ApplicationConfiguration.java
@@ -5,8 +5,14 @@ package com.sp25group8.swipe4mebackend.configs;
 
 import com.sp25group8.swipe4mebackend.users.UserRepository;
 import lombok.AllArgsConstructor;
+
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -42,6 +48,23 @@ public class ApplicationConfiguration {
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(System.getenv("MAIL_USERNAME"));
+        mailSender.setPassword(System.getenv("MAIL_PASSWORD"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 
 }
